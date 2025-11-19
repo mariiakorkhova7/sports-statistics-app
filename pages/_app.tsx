1fb1f335ecb2d545cb2c5ff7c1c2f67a891519e8
mariiakorkhova7 <207155588+mariiakorkhova7@users.noreply.github.com>
2025-11-19 17:03:51 +0200

    feat(ui): Add Global AuthProvider and Navbar layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { AuthProvider } from '../lib/AuthContext';
+import { AuthProvider } from '@/lib/AuthContext';
+import Navbar from '@/components/Navbar';
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
+      <Navbar />
       <Component {...pageProps} />
     </AuthProvider>
   );
 }
-
-export default MyApp;
