23e1d3f95854820bdbf3fd9e8e9c258bd3550c4f
mariiakorkhova7 <207155588+mariiakorkhova7@users.noreply.github.com>
2025-11-25 22:06:41 +0200

    fix(ui): Change main page footer to a premade component

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { useAuth } from '@/lib/AuthContext';
+import Footer from '@/components/Footer';
 
 interface Tournament {
   id: number;
@@ -116,14 +117,7 @@ export default function Home() {
         )}
 
       </main>
-      <footer className="bg-white border-t py-5 mt-auto">
-  <div className="max-w-6xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center text-gray-500 text-sm">
-    <div className="flex gap-4 mt-4 md:mt-0">
-      <Link href="#" className="hover:text-gray-900">Про нас</Link>
-    </div>
-    <p>&copy; ShuttleStats 2025</p>
-  </div>
-</footer>
+      <Footer />
     </div>
   );
 }
