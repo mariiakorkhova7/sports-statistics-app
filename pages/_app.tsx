ee0c83ed106e941e8367ae4483ad84a336711392
mariiakorkhova7 <207155588+mariiakorkhova7@users.noreply.github.com>
2025-11-30 15:57:55 +0200

    feat(ui): Add Noto Sans font to ensure consistent cross-platform rendering

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,21 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { AuthProvider } from '@/lib/AuthContext';
 import Navbar from '@/components/Navbar';
+import { Noto_Sans } from 'next/font/google';
+
+const font = Noto_Sans({ 
+  subsets: ['latin', 'cyrillic'], 
+  variable: '--font-main',
+  display: 'swap',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <Navbar />
-      <Component {...pageProps} />
+      <main className={font.className}>
+        <Navbar />
+        <Component {...pageProps} />
+      </main>
     </AuthProvider>
   );
 }
